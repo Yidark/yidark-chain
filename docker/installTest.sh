8ba4fcb701a75eb50c0100f3aacbad14ee8fb81d
tech <tech@yidark.io>
2023-05-15 16:20:05 +0800

    add test sh

--- docker/installTest.sh
+++ docker/installTest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 if ! type docker >/dev/null 2>&1; then
     wget -qO- https://get.docker.com/ | sh
     mkdir -p /etc/docker
@@ -8,19 +8,14 @@ if ! type docker >/dev/null 2>&1; then
 fi
 
 docker pull xiezhanzhang/yidark-chain:test
-# 运行出快节点
+
 if [ $# -eq 3 ] ; then
-    cmd="docker run -d --name ydk-miner-chain -v /ydk/minerData:/app/data  -p 8545:8545 xiezhanzhang/yidark-chain:test ${1} ${2} ${3}"
-    $cmd
+    docker run -d --name ydk-miner-chain -v /ydk/minerData:/app/data  -p 8546:8545 xiezhanzhang/yidark-chain:test "${1}" ${2} ${3}
 fi
 
 # 运行普通节点
 if [ $# -eq 1 ] ; then
-    cmd="docker run -d --name ydk-chain -v /ydk/data:/app/data  -p 8545:8545 xiezhanzhang/yidark-chain:test '--networkid 668 --http --http.addr 0.0.0.0 --http.api txpool,db,eth,web3,net,personal,admin --http.corsdomain * --port 30303 --http.port 8545 --syncmode=fast  --allow-insecure-unlock  --verbosity 3 --http.vhosts=* ${1}'"
-    $cmd
+    pms="--networkid 668 --http --http.addr 0.0.0.0 --http.api txpool,db,eth,web3,net,personal,admin --port 30303 --http.port 8545 --syncmode=fast  --allow-insecure-unlock  --verbosity 3 --http.vhosts=* "
+    pm="${pms} ${1}"
+    docker run -d --name ydk-chain -v /ydk/data:/app/data  -p 8545:8545 xiezhanzhang/yidark-chain:test "${pm}"
 fi
-
-
-
-
-
