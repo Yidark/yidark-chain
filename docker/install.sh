44bf47fc83c8a205022e9a22ef1ae231ebcef569
tech <tech@yidark.io>
2023-07-01 10:22:36 +0800

    Add official version

--- docker/install.sh
+++ docker/install.sh
@@ -10,11 +10,11 @@ fi
 docker pull yidark/yidark-chain:test
 
 if [ $# -eq 3 ] ; then
-    docker run -d --name ydk-miner-chain -v /ydk/minerData:/app/data  -p 8546:8545 -p 30304:30303  yidark/yidark-chain "${1}" ${2} ${3}
+    docker run -d --name ydk-miner-chain -v /ydk/minerData:/app/data  -p 8546:8545 -p 30304:30303  yidark/yidark-chain:v2 "${1}" ${2} ${3}
 fi
 
 if [ $# -eq 1 ] ; then
     pms="--http --http.addr 0.0.0.0 --http.api eth,web3,net --port 30303 --http.port 8545 --verbosity 3 --http.vhosts=* "
     pm="${pms} ${1}"
-    docker run -d --name ydk-chain -v /ydk/data:/app/data  -p 8545:8545 -p 30303:30303 yidark/yidark-chain "${pm}"
+    docker run -d --name ydk-chain -v /ydk/data:/app/data  -p 8545:8545 -p 30303:30303 yidark/yidark-chain:v2 "${pm}"
 fi
