3c6e07349df6dc65f06806dfb4863c3f42316d9f
Yidark Chain <118953587+Yidark@users.noreply.github.com>
2024-01-03 11:04:18 +0800

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -7,14 +7,14 @@ if ! type docker >/dev/null 2>&1; then
     systemctl restart docker
 fi
 
-docker pull yidark/yidark-chain:v7
+docker pull yidark/yidark-chain:v8
 
 if [ $# -eq 3 ] ; then
-    docker run -d --name ydk-miner-chain -v /ydk/minerData:/app/data  -p 8546:8545 -p 30304:30303  yidark/yidark-chain:v3 "${1}" ${2} ${3}
+    docker run -d --name ydk-miner-chain -v /ydk/minerData:/app/data  -p 8546:8545 -p 30304:30303  yidark/yidark-chain:v8 "${1}" ${2} ${3}
 fi
 
 if [ $# -eq 1 ] ; then
     pms="--http --http.addr 0.0.0.0 --http.api eth,web3,net --port 30303 --http.port 8545 --verbosity 3 --http.vhosts=* "
     pm="${pms} ${1}"
-    docker run -d --name ydk-chain -v /ydk/data:/app/data  -p 8545:8545 -p 30303:30303 yidark/yidark-chain:v3 "${pm}"
+    docker run -d --name ydk-chain -v /ydk/data:/app/data  -p 8545:8545 -p 30303:30303 yidark/yidark-chain:v8 "${pm}"
 fi
